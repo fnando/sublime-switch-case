74ae541344468ad385fbdb2e452026112ebb8a04
Nando Vieira <fnando.vieira@gmail.com>
2014-02-22 01:34:43 -0300

    Switch from snake_case to CamelCase first.

--- SwitchCase.py
+++ SwitchCase.py
@@ -6,6 +6,9 @@ def is_camelcase(text):
 def is_snake_case(text):
   return re.match("^[a-z]+(_?[a-z0-9]+)*$", text)
 
+def is_scream_snake_case(text):
+  return re.match("^[A-Z]+(_?[A-Z0-9]+)*$", text)
+
 def convert_to_camelcase(text):
   return "".join(map(lambda w: w[:1].upper() + w[1:].lower(), text.split("_")))
 
@@ -20,9 +23,9 @@ class SwitchCaseCommand(sublime_plugin.TextCommand):
     word_region = self.view.word(self.view.sel()[0])
     word = self.view.substr(word_region)
 
-    if is_camelcase(word):
-      self.view.replace(edit, word_region, convert_to_snake_case(word))
-    elif is_snake_case(word):
-      self.view.replace(edit, word_region, convert_to_scream_snake_case(word))
-    else:
+    if is_snake_case(word):
       self.view.replace(edit, word_region, convert_to_camelcase(word))
+    elif is_camelcase(word):
+      self.view.replace(edit, word_region, convert_to_scream_snake_case(convert_to_snake_case(word)))
+    else:
+      self.view.replace(edit, word_region, convert_to_snake_case(word))
